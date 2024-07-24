27af271f96f026d3c988fc3d4e0557ff174d15ee
Rakesh Kumar <thakurmayank0508@gmail.com>
2024-07-24 12:27:18 +0530

    cpp-challenge

--- samplecode/firstcode.cpp
+++ samplecode/firstcode.cpp
@@ -5,5 +5,6 @@ int main(){
     std::cout<<"Provide number plz";
     std::cin >> fav_no;
     std::cout <<"This is my fav number23 aswell"<<std::endl;
+    std::cout <<"No really "<<fav_no<<" is my fav number"<<std::endl;
     return 0;
 }
