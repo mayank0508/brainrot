35dcf104e2c17ee5aa563f2da98e9dd782ca2540
Rakesh Kumar <thakurmayank0508@gmail.com>
2024-08-06 12:25:33 +0530

    cpp

---
+++ samplecode/variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int room_width {100};
+int room_breadth {200};
+int area = room_breadth * room_width;
+cout << area <<endl;
+
+
+    return 0;
+}
