1dc39b61e5f8dc8e32b3c729c873356f2556b894
Rakesh Kumar <thakurmayank0508@gmail.com>
2024-08-06 12:29:22 +0530

    cpp

--- samplecode/variable.cpp
+++ samplecode/variable.cpp
@@ -1,20 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-int room_width {0};
-int room_breadth {0};
+    cout << "Enter the width of the room" << endl;
+    int room_width{0};
+    cin >> room_width;
 
-cout<< "Enter the width of the room"<<endl;
-cin>>room_width;
+    cout << "Enter the breadth of the room" << endl;
+    int room_breadth{0};
+    cin >> room_breadth;
 
-cout<< "Enter the breadth of the room"<<endl;
-cin>>room_breadth;
-
-
-cout << "The Total Area of this room is "<< room_breadth * room_width <<" Square feets."<<endl;
-
+    cout << "The Total Area of this room is " << room_breadth * room_width << " Square feets." << endl;
 
     return 0;
 }
