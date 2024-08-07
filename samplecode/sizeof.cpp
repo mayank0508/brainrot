2960845a8281ea30d2e41e5b4988c12f2bdf27e6
Rakesh Kumar <thakurmayank0508@gmail.com>
2024-08-07 12:07:31 +0530

    cpp

---
+++ samplecode/sizeof.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+
+cout<<"+++++++++++++++++++++++++++++++"<<endl;
+cout<<" The Size of float is "<<sizeof(float)<<endl;
+cout<<" The Size of Double is "<<sizeof(double)<<endl;
+cout<<" The Size of short is "<<sizeof(short)<<endl;
+cout<<" The Size of long is "<<sizeof(long)<<endl;
+cout<<" The Size of long long is "<<sizeof(long long)<<endl;
+cout<<" The Size of float is "<<sizeof(float)<<endl;
+
+return 0;
+}
