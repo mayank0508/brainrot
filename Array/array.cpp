73b0d4636746571e7d8bf9bfb0a15f2374f9b384
Rakesh Kumar <thakurmayank0508@gmail.com>
2024-08-08 12:25:15 +0530

    cpp-array

--- Array/array.cpp
+++ Array/array.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    //array
-    int array_one [12];
-    const array_two {100};
+int main()
+{
+    // array
+    int array_one[12];
+    const array_two{100};
     return 0;
+
+    char vowels[] {'a','e','i','o','u'};
+    cout <<"\n The first vowel is "<<vowel[0] <<endl;
+    cout <<"The laste vowel is "<<vowel[4] <<endl;
+
+
 }
